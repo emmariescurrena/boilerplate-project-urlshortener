2126b3fa2ba9a3ca91acbf6907e1862e78f1352f
emmariescurrena <125983228+emmariescurrena@users.noreply.github.com>
2024-08-11 10:46:37 -0300

    Rejects invalid urls and returns original_url

--- index.js
+++ index.js
@@ -10,15 +10,31 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+    res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
+
+app.use(express.urlencoded());
+
+app.post('/api/shorturl', (req, res) => {
+    const originalUrl = req.body.url
+    if (!originalUrl.match(/^https:\/\/.{2,}\.com$/gm)) {
+        res.json({
+            error: 'invalid url'
+        })
+        return;
+    }
+    res.json({
+        original_url: originalUrl,
+        short_url: '',
+    })
+})
